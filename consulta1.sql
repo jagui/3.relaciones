6671a59de5185f2eb7aeafb7c86731bf224df8eb
Juan Agüí <juan@juanagui.com>
2021-03-14 19:30:43 +0100

    removes solutions

--- consulta1.sql
+++ consulta1.sql
@@ -1,2 +1,1 @@
 -- haz una consulta que muestre todas las playlists de la base de datos
-SELECT * FROM playlists;
--- consulta2.sql
+++ consulta2.sql
@@ -1,7 +1,2 @@
 -- escribe una consulta que liste todas las canciones de una playlist
 -- Si tienes muchos resultados, puedes añadir LIMIT X al final de la consulta, donde X es el número de filas que se devuelven
-SELECT * FROM playlists 
-JOIN playlist_track ON playlist_track.playlistid == playlists.playlistid 
-JOIN tracks ON tracks.trackId == playlist_track.trackId
-WHERE playlists.playlistId==5
-LIMIT 20;
